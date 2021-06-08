53a47b05c06444d733aacf7f37f758abee93b67b
yj-ac <yj_ac@icloud.com>
2021-06-08 18:35:33 +0900

    type select

--- app.py
+++ app.py
@@ -6,33 +6,36 @@ import japanize_matplotlib
 import streamlit as st
 from streamlit.caching import cache
 
-@cache
-def create_plot_data(file,shohin_code):
-    
+@st.cache
+def create_plot_data(file,code,select):
+
     df = pd.read_csv(file,encoding='cp932')
     df = df[df['店舗']!='合計']
-    df = df[df['売上金額']!=0]
-    df['商品コード'] = df['商品コード'].astype('str').str.lstrip('4')
+    df = df[df[select]!=0]
+    df['商品コード'] = df['商品コード'].astype('str').str[-6:]
     
-    df = df[df['商品コード']==shohin_code]
+    df = df[df['商品コード']==code]
     
     x = df['平均単価']
-    y = df['売上金額']
+    y = df[select]
     
     return x,y,df
-st.title('売上金額散布図')
+    
+st.title(' 売上金額散布図')
 df = st.file_uploader('ファイル選択',type='csv')
+select = st.selectbox('選択',['売上金額','売上金額PI','売上数量'])
 
 if df is not None:
-    shohin_code = st.text_input('商品コードを入力',max_chars=6)
-    if shohin_code is not '':
-        x,y,df = create_plot_data(df,shohin_code)
-        name = df[df['商品コード']==shohin_code]['商品名称'].iloc[1]
-
+    code = st.text_input('商品コードを入力:半角6桁',max_chars=6)
+    if code is not '':
+    #for code in ['194700', '087386']:
+        x,y,df = create_plot_data(df,code,select)
+        name = df[df['商品コード']==code]['商品名称'].iloc[1]
+                
         fig = plt.figure(figsize=(12,9))
         plt.scatter(x,y)
         plt.xlabel('平均単価')
-        plt.ylabel('売上金額')
+        plt.ylabel(select)
         plt.title(name)
 
         st.pyplot(fig)
